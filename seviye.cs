8ed78f1f763583da66f895e4e5692911f2493ab5
Berke Erdem <berkeerdem0@gmail.com>
2023-01-29 18:43:44 +0300

    Level Code

---
+++ seviye.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class seviye : MonoBehaviour
+{
+	public GameObject platformObjesi;
+	public int platformSayisi;
+	public float minX,maxX;
+	public float minY,maxY;
+
+
+    void Start()
+    {
+		Vector3 klonpozisyonu=new Vector3();
+		for(int i=0;i<platformSayisi;i++){
+
+			klonpozisyonu.x=Random.Range(minX,maxX);
+			klonpozisyonu.x=Random.Range(minY,maxY);
+			Instantiate (platformObjesi,klonpozisyonu,Quaternion.identity);//klonlama kodu:klonlanacak obje,pozisyonu,açısı		}
+    	}
+
+   
+	}
+}
