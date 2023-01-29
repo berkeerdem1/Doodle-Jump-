35fe30d9a5b79920c1dee6e4f51eac3f11700339
Berke Erdem <berkeerdem0@gmail.com>
2023-01-29 18:45:18 +0300

    Clear

---
+++ temizle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class temizle : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D temas){
+
+		float rastgeleX=Random.Range(-5f,5f);
+		float rastgeleY=15f;
+
+		if(temas.tag =="platform"){
+			temas.transform.position=new Vector3(rastgeleX,temas.transform.position.y+rastgeleY,temas.transform.position.z);//SONSUZLUK KODU
+
+		}
+	}
+}
